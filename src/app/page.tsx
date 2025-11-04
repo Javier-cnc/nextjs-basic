dc365af9af77dfd13dfe37c8b18663d4bbfdf5ed
Javier-cnc <grumpyjg95@gmail.com>
2025-11-03 23:31:44 -0500

    primer commit mio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,10 @@ export default function Home() {
           priority
         />
       </div>
+      <div className="bg-red-800 text-white">
+        Esto es solo un texto que necesito publicar para poder probar cambios
+        que haga en el vuelo
+      </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
